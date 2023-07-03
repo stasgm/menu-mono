1bc89498cfd74faa96ffdcce8ad0301290b55c7f
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-07-03 16:37:41 +0300

    Update

--- configs/prettier/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const eslintBaseConfig = require("@configs/eslint/eslint.base");
-
-module.exports = {
-  ...eslintBaseConfig,
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: "tsconfig.json",
-  },
-};
