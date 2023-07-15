b9a592dcc833b8169ed029872b1e403e08faa363
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-07-15 14:29:38 +0300

    Update linter config

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,6 +2,10 @@ const eslintBaseConfig = require('@configs/nest-eslint');
 
 module.exports = {
   ...eslintBaseConfig,
+  ignorePatterns: [
+    ...eslintBaseConfig.ignorePatterns,
+    'generate-typings.ts',
+  ],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.json',
