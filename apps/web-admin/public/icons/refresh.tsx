67b37f5ff89e3ff5409826ae3b50be7edb1d6f4c
hirurg-lybitel <yurashoihet@gmail.com>
2023-08-13 01:45:44 +0300

    Update refresh.tsx

--- apps/web-admin/public/icons/refresh.tsx
+++ apps/web-admin/public/icons/refresh.tsx
@@ -8,9 +8,9 @@ export const RefreshIcon = () => (
   >
     <path
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       d="M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97"
     />
   </svg>
