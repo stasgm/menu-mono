7be01abb09ab84d220031028d1fc6040fb864628
Stanislau Shliakhtsich <stasgm@gmail.com>
2024-02-07 20:01:01 +0300

    Refactor formatError

--- apps/api/src/core/config/graphql-config.ts
+++ apps/api/src/core/config/graphql-config.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path';
 
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/dist/esm/plugin/landingPage/default';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { HttpException, Injectable } from '@nestjs/common';
 import { GqlOptionsFactory } from '@nestjs/graphql';
@@ -17,22 +17,16 @@ export class GraphqlConfigService implements GqlOptionsFactory {
 
   private formatError(formattedError: GraphQLFormattedError, error: unknown) {
     const message = formattedError.message.replace('Validation error: ', '').replace('GraphQL error: ', '');
+    const name = formattedError.extensions?.code
+    const originalError = formattedError?.extensions?.originalError as Record<string, unknown>;
 
-    const originalError: any = formattedError?.extensions?.originalError;
-
-    const type = originalError.type;
-    const code = originalError.statusCode;
+    const code = originalError?.statusCode ?? formattedError.extensions?.status ?? 500;
 
     if (error instanceof GraphQLError) {
       return {
-        message: error.message,
-        name: formattedError.extensions?.code,
-        code: formattedError.extensions?.status || 500,
-        extensions: {
-          type,
-          code,
-          message,
-        },
+        message,
+        name,
+        code
       };
     } else if (error instanceof HttpException) {
       return {
@@ -47,28 +41,11 @@ export class GraphqlConfigService implements GqlOptionsFactory {
 
   createGqlOptions(): ApolloDriverConfig {
     return {
-      // formatError(formattedError, error) {
-      //   if (error instanceof GraphQLError) {
-      //     return {
-      //       message: error.message,
-      //       name: formattedError.extensions?.code,
-      //       code: formattedError.extensions?.status || 500,
-      //     };
-      //   } else if (error instanceof HttpException) {
-      //     return {
-      //       message: error.message,
-      //       name: error.name,
-      //       code: 500,
-      //     };
-      //   }
-      //   return formattedError;
-      // },
       formatError: (formattedError, error) => this.formatError(formattedError, error),
       autoSchemaFile: join(process.cwd(), 'src/types/schema.gql'),
-      sortSchema: true,
-      // typePaths: ['./**/*.graphql'],
+      // sortSchema: true,
       transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
-      installSubscriptionHandlers: true,
+      // installSubscriptionHandlers: true,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       includeStacktraceInErrorResponses: process.env.NODE_ENV === 'development',
